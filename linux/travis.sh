9a9573eee654b36618c7da396621304588214205
Filip Hracek <filip.hracek@gmail.com>
2016-08-02 22:54:08 -0700

    Different approach

--- linux/travis.sh
+++ linux/travis.sh
@@ -3,6 +3,6 @@
 current=dart_1.18.1-1_amd64.deb
 
 wget "https://storage.googleapis.com/dart-archive/channels/stable/release/latest/linux_packages/debian_wheezy/$current"
-dpkg -i $current --force-not-root --root=$HOME
+dpkg -i $current --force-not-root --root=.
 ls -la
 
