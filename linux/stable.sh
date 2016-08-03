19196e174b945f381f732b9cfdc4de87ae768d67
Filip Hracek <filip.hracek@gmail.com>
2016-08-02 22:29:32 -0700

    Remove sudo

--- linux/stable.sh
+++ linux/stable.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sudo apt-get -qq update
-sudo apt-get install -y apt-transport-https
-sudo sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
-sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
-sudo apt-get -qq update
-sudo apt-get install -y dart/stable
+apt-get -qq update
+apt-get install apt-transport-https
+sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+apt-get -qq update
+apt-get install dart/stable
