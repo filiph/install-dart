df9b0931b3ba9753ade8bc4d7f807a02c2adc399
Filip Hracek <filip.hracek@gmail.com>
2016-08-02 22:41:40 -0700

    Try installing .deb

---
+++ linux/travis.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+current=dart_1.18.1-1_amd64.deb
+
+wget "https://storage.googleapis.com/dart-archive/channels/stable/release/latest/linux_packages/debian_wheezy/$current"
+dpkg -i $current
+ls -la
+
